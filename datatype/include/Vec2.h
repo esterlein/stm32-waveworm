ab279a4a0efa4438456569cbcef009a5f6317f5b
esterlein <esterlein.git@gmail.com>
2024-01-22 23:45:43 -0500

    add 2d vector base

--- datatype/include/Vec2.h
+++ datatype/include/Vec2.h
@@ -1,1 +1,14 @@
 #pragma once
+
+
+template <class T>
+class Vec2
+{
+public:
+    T X;
+    T Y;
+
+    Vec2() : X{0}, Y{0} {}
+    Vec2(T x, T y) : X{x}, Y{y} {}
+    explicit Vec2(T n) : X{n}, Y{n} {}
+};
