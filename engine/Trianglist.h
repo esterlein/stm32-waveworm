cd7c54a0e965045e75eacde1098207b453eefce6
esterlein <esterlein.git@gmail.com>
2024-03-19 21:51:31 -0400

    add trianglist fields

--- engine/Trianglist.h
+++ engine/Trianglist.h
@@ -3,9 +3,13 @@
 #include <array>
 
 
-template <typename T, size_t N>
+template<typename T, size_t N>
 class Trianglist
 {
+private:
+    std::array<T, N> m_vtxs;
+    std::array<size_t, N> m_idxs;
+
 public:
     Trianglist() = default;
 };
